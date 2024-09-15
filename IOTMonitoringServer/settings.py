55f0f4f31ea03434d580aee67cbf84ff6c47480c
Fredy Alarcon <f.alarconf@uniandes.edu.co>
2024-09-15 10:19:01 -0500

    Cambio de ips de la VM ips públicas

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "18.215.34.226"]
+ALLOWED_HOSTS = ["localhost", "3.92.152.212"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.238.153.167",  # Dirección IP de la base de datos
+        "HOST": "3.236.37.84",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "35.153.49.44"
+MQTT_HOST = "100.27.8.247"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
