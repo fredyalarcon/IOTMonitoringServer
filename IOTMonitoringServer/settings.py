ba9636d668fc747ea349cb67be35c64d3368cacd
Fredy Alarcon <f.alarconf@uniandes.edu.co>
2024-09-16 20:20:18 -0500

    Nuevas ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "44.192.1.200"]
+ALLOWED_HOSTS = ["localhost", "34.205.75.200"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.235.91.79",  # Dirección IP de la base de datos
+        "HOST": "44.192.100.26",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.205.155.179"
+MQTT_HOST = "98.80.168.44"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
